3609c6afb911a886844c2516b795a4423c55be50
K-One19 <62478650+K-One19@users.noreply.github.com>
2022-09-27 10:09:23 +0000

    Add files via upload

---
+++ nex_vote_beta-master/lib/department_candidates/positions_aces/gen_sec.dart
@@ -0,0 +1,99 @@
+//DISPLAYING THE GENERAL SECRETARY CANDIDATES FOR ACES
+
+import 'package:flutter/material.dart';
+
+class GenSec extends StatefulWidget {
+  const GenSec({Key? key}) : super(key: key);
+
+  @override
+  State<GenSec> createState() => _GenSecState();
+}
+
+class _GenSecState extends State<GenSec> {
+  int selectedValue = 4;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: ListView(
+        padding: EdgeInsets.symmetric(vertical: 16),
+        children: [
+          const SizedBox(height: 50),
+
+          //1ST
+          Container(
+            decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
+            child: RadioListTile(
+              value: 0,
+              groupValue: selectedValue,
+              title: Text(
+                'Rexford Machu',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text('Lets do this!'),
+              secondary: ClipRRect(
+                borderRadius: BorderRadius.circular(27),
+                child: Image.asset('assets/images/president1.jpeg'),
+              ),
+              onChanged: (int? value) {
+                setState(() {
+                  selectedValue = value!;
+                });
+              },
+            ),
+          ),
+          const SizedBox(height: 20),
+
+          //2ND
+          Container(
+            decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
+            child: RadioListTile(
+              value: 1,
+              groupValue: selectedValue,
+              title: Text(
+                'Marcel Nortey',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text('Of course we can!'),
+              secondary: ClipRRect(
+                borderRadius: BorderRadius.circular(27),
+                child: Image.asset('assets/images/president2.jpeg'),
+              ),
+              onChanged: (int? value) {
+                setState(() {
+                  selectedValue = value!;
+                });
+              },
+            ),
+          ),
+          const SizedBox(height: 20),
+
+          //3RD
+          Container(
+            decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
+            child: RadioListTile(
+              value: 2,
+              groupValue: selectedValue,
+              title: Text(
+                'Jeffrey Ofori Kwakye',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text('I did it!'),
+              secondary: ClipRRect(
+                borderRadius: BorderRadius.circular(27),
+                child: Image.asset('assets/images/mypic.JPG'),
+              ),
+              onChanged: (int? value) {
+                setState(() {
+                  selectedValue = value!;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
